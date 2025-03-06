8bc198cdf92c1753df3d9b92f869db292e0e515c
Shohag Mahmud <shohagwata@gmail.com>
2025-03-06 10:51:42 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from app import mysql
+
+def init_db():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            task VARCHAR(255) NOT NULL
+        )
+    ''')
+    mysql.connection.commit()
 
