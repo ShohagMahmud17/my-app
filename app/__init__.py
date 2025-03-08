625c3417266791a6cdef456b0a001a1bae1780f0
Shohag Mahmud <shohagwata@gmail.com>
2025-03-08 19:53:13 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
-from task_manager import models
-models.init_db()
+from flask import Flask
+from app.database import init_db
+
+app = Flask(__name__)
+
+# Initialize database on startup
+init_db()
+
+
 
